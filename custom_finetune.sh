b6cd85f894c8dfae8bd8636c9605beffdd618fed
eshansurendra <janukaeshan@gmail.com>
2024-07-12 01:34:13 +0530

    updated tpu

--- custom_finetune.sh
+++ custom_finetune.sh
@@ -3,7 +3,7 @@ IMAGE_PATH="/kaggle/working/svgtopng_dataset/images"
 MODEL_MAX_LENGTH=3072
 OUTPUT_DIR="/kaggle/working/custom-finetune-TinyLLaVA-Phi-2-SigLIP-3.1B-lora"
 
-deepspeed --include localhost:0,1 --master_port 29501 /kaggle/working/TinyLLaVA_Factory/tinyllava/train/custom_finetune.py \
+deepspeed --include /device:TPU:0,/device:TPU:1,/device:TPU:2,/device:TPU:3,/device:TPU:4,/device:TPU:5,/device:TPU:6,/device:TPU:7 --master_port 29501 /kaggle/working/TinyLLaVA_Factory/tinyllava/train/custom_finetune.py \
     --deepspeed /kaggle/working/TinyLLaVA_Factory/scripts/zero2.json \
     --data_path  $DATA_PATH \
     --image_folder $IMAGE_PATH \
